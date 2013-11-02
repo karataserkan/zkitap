fde590d04ff5247d9f26c214dc2c159e1b885b13
Uğur Dökmeci <ugur@lindneo.com>
2013-11-02 09:54:04 +0000

    Login MD5 problem solved

--- protected/models/LoginForm.php
+++ protected/models/LoginForm.php
@@ -34,7 +34,7 @@ class LoginForm extends CFormModel
 	 * Declares attribute labels.
 	 */
 	public function attributeLabels()
-	{
+	{ 
 		return array(
 			'rememberMe'=>'Remember me next time',
 		);
@@ -48,7 +48,7 @@ class LoginForm extends CFormModel
 	{
 		if(!$this->hasErrors())
 		{
-			$this->_identity=new UserIdentity($this->email,$this->password);
+			$this->_identity=new UserIdentity($this->email,md5(sha1($this->password)));
 			if(!$this->_identity->authenticate())
 				$this->addError('password','Incorrect username or password.');
 		}
