b1f2485eaa28ab57848ca811c29f3bc45ae14d5b
root <root@lindneo.com>
2013-11-02 09:26:59 +0000

    Initial commit

---
+++ protected/views/book/_form.php
@@ -0,0 +1,60 @@
+<?php
+/* @var $this BookController */
+/* @var $model Book */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'book-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+		<?php echo $form->error($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'title'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'author'); ?>
+		<?php echo $form->textField($model,'author',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'author'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'created'); ?>
+		<?php echo $form->textField($model,'created'); ?>
+		<?php echo $form->error($model,'created'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'publish_time'); ?>
+		<?php echo $form->textField($model,'publish_time'); ?>
+		<?php echo $form->error($model,'publish_time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'data'); ?>
+		<?php echo $form->textField($model,'data'); ?>
+		<?php echo $form->error($model,'data'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/book/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this BookController */
+/* @var $data Book */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('author')); ?>:</b>
+	<?php echo CHtml::encode($data->author); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created')); ?>:</b>
+	<?php echo CHtml::encode($data->created); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('publish_time')); ?>:</b>
+	<?php echo CHtml::encode($data->publish_time); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('data')); ?>:</b>
+	<?php echo CHtml::encode($data->data); ?>
+	<br />
+
+
+</div>
