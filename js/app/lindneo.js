4e44300d6012656ee45fdb4286d8cb67e7b93629
Uğur Dökmeci <ugur@lindneo.com>
2013-11-14 09:25:44 +0000

    JS path index olarak ayarlandı

--- js/app/lindneo.js
+++ js/app/lindneo.js
@@ -2,7 +2,7 @@
 // all global variables and the things other than module should be defined here.
 window.lindneo = (function(window){
 
-  var url = 'http://ahmet.dev.lindneo.com/index.php';
+  var url = '/index.php';
   var currentPageId = 'Ip38yh5mNn2uzH6JTwmO29oI65Qy7c745YskdqualT85';
   var currentComponent = {};
 
