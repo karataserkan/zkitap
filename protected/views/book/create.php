334e386aca68ce10f50a0e131064d04fad7f578a
www-data <www-data@mail.lindneo.com>
2013-12-31 08:51:46 +0000

    book create updated

--- protected/views/book/create.php
+++ protected/views/book/create.php
@@ -7,10 +7,10 @@ $this->breadcrumbs=array(
 	'Create',
 );
 
-$this->menu=array(
-	array('label'=>'List Book', 'url'=>array('index')),
-	array('label'=>'Manage Book', 'url'=>array('admin')),
-);
+// $this->menu=array(
+// 	array('label'=>'List Book', 'url'=>array('index')),
+// 	array('label'=>'Manage Book', 'url'=>array('admin')),
+// );
 ?>
 
  
