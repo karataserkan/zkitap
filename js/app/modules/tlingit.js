e6378efd62db01e993e7b202cc2f50b99d9f878d
reset <reset>
2014-02-06 13:56:11 +0000

    pdf preview

--- js/app/modules/tlingit.js
+++ js/app/modules/tlingit.js
@@ -139,6 +139,7 @@ window.lindneo.tlingit = (function(window, $, undefined){
         function(err){
           console.log('error:' + err);
       });
+
       
 
   };
@@ -147,19 +148,45 @@ window.lindneo.tlingit = (function(window, $, undefined){
     $("li.page").each(function(index, pageSlice){
       //console.log(pageSlice);
 
-      var pagePreview = $('<canvas class="preview" height="90" width="120"> </canvas>');
+      var pagePreview = $('<canvas class="preview"  height="90"  width="120"> </canvas>');
     
       $(pageSlice).children('.preview').remove();
       $(pageSlice).prepend(pagePreview);
       var canvas=$(pageSlice).children('.preview')[0];
       var context=canvas.getContext("2d");
-       context.fillStyle = '#FFF';
-        context.fillRect(0,0,canvas.width,canvas.height);
-        
+     context.fillStyle = '#FFF';
+      context.fillRect(0,0,canvas.width,canvas.height);
+      console.log('ddedededede');
+      //$('.chapter .page').each(function(){
+      //console.log($(this).attr('page_id'));
+      
+
+      $.ajax({
+          type: "POST",
+          url:'/page/getPdfThumbnail?pageId='+$(this).attr('page_id'),
+        }).done(function(page_data){
+          
+          var page_background = JSON.parse(page_data);
+          //console.log(page_background.result);
+          if(page_background.result){
+            /*var background_img = '<img src="'+page_background.result+'" id="canvas_'+$(this).attr('page_id')+'"  height="90" width="120" style="display:none"/>';
+            $(pageSlice).prepend(background_img);
+            var background = document.getElementById('canvas_'+$(this).attr('page_id'));
+            context.drawImage(background, 120,90);*/
+            var img = new Image();
+            img.src = page_background.result;
+            img.onload = function(){
+              context.drawImage(img, 0, 0);
+            };
+
+          }
+        });
+      //});
     });
    $('.chapter .page').each(function(){
+    console.log($(this).attr('page_id'));
     loadPagesPreviews($(this).attr('page_id'));
-   })
+    });
   };
 
   var loadPagesPreviews = function (pageId) {
@@ -179,8 +206,7 @@ window.lindneo.tlingit = (function(window, $, undefined){
   };
 
   var PreviewOfPage = function (response) {
-
-
+//console.log(response);
     if ($.isEmptyObject(responseFromJson(response).result)) return false;
 
     var components= responseFromJson(response).result.components;
