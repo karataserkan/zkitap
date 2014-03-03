6ce6ed4d72c929dc42c11aaad127bf54872c1b5e
reset <reset>
2014-03-03 15:42:45 +0000

    tag component

--- protected/views/book/author.php
+++ protected/views/book/author.php
@@ -635,6 +635,41 @@ $current_user=User::model()->findByPk(Yii::app()->user->id);
 </div>	
 <!--  end add galery popup -->	
 
+<!--  add tag popup -->	
+<div class="popup" id="pop-galery-popup">
+<div class="popup-header">
+	<i class="icon-m-galery"></i>
+		Tag Ekle
+	<i id="image-add-dummy-close-button" class="icon-close size-10" style="float:right; margin-right:10px; margin-top:5px;"></i>
+</div>
+<!-- popup content-->
+	<div class="gallery-inner-holder">
+		<div style="clear:both"></div>
+			<div style="margin-bottom:20px;">
+				<label class="dropdown-label" id="leading">
+						Görsel Adedi:
+							<select id="leading" class="radius">
+								<option selected="" value="8"> 1 </option>
+								<option value="0" >2</option>
+								<option value="10" >3</option>
+								<option value="20" >4</option>
+								<option value="30" >5</option>
+								<option value="40" >6</option>
+								<option value="50" >7</option>
+								<option value="60" >8</option>
+								<option value="70" >9</option>
+								<option value="80" >10</option>
+							</select>	
+					</label>
+					
+			</div>
+			<div class="add-image-drag-area"> </div>
+		<a href="#" class="btn btn-info" id="add-image" style="padding: 5px 30px;">Ekle</a>
+	</div>
+<!-- popup content-->
+</div>	
+<!--  end add galery popup -->	
+
 <!--  add slider popup -->	
 <div class="popup" id="pop-galery-popup">
 <div class="popup-header">
@@ -922,6 +957,7 @@ $current_user=User::model()->findByPk(Yii::app()->user->id);
 			
 			<li ctype="galery" class="component icon-m-galery">&nbsp;&nbsp;&nbsp;&nbsp;Galeri</li>
 			<li ctype="slider" class="component icon-m-galery">&nbsp;&nbsp;&nbsp;&nbsp;Slider</li>
+			<li ctype="tag" class="component icon-m-galery">&nbsp;&nbsp;&nbsp;&nbsp;Tag</li>
 			<li ctype="quiz"  class="component icon-m-quiz">&nbsp;&nbsp;&nbsp;&nbsp;Quiz</li>
 			<li ctype="side-text"  class="component icon-m-listbox">&nbsp;&nbsp;&nbsp;Yazı Kutusu</li>
 			<li ctype="link" class="component icon-m-link ui-draggable">&nbsp;&nbsp;&nbsp;&nbsp;Link</li>
--- protected/views/layouts/author.php
+++ protected/views/layouts/author.php
@@ -103,7 +103,8 @@
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/app/components/html-component.js"></script>	
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/app/components/wrap-component.js"></script>	
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/app/components/latex-component.js"></script>
-		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/app/components/slider-component.js"></script>	
+		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/app/components/slider-component.js"></script>
+		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/app/components/tag-component.js"></script>	
 
 		<!-- Page JS Codes -->
 		<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/app/other/page-drag-drop.js"></script>
