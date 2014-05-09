2adc6a48aeb2d53bf9f8d7ef8c18b62c8a125e3d
reset <reset>
2014-05-09 12:42:38 +0000

    Editor UI Development goes on

--- protected/views/organisations/account.php
+++ protected/views/organisations/account.php
@@ -19,61 +19,39 @@
 	</div>
 	<!-- /PAGE HEADER -->
 	<div class="row">
-
-
-        <div class="myprofile_information_container">
-        
-        <div class="myprofile_picture_container">
-        <div class="change_profile_picture"><a href="#"><i class="fa fa-edit"></i></a></div>
-        <img src="/css/images/avatar.png">
-        </div>
-        
-        
-        
-        <div class="myprofile_information_part">
-        
-        <div class="myprofile_information_components">
-        <p>HESABIN</p>
-        <button class="btn btn-success">Değişiklikleri Kaydet</button>
-        </div>
-        
-        <div class="myprofile_information_components">
-        <p>İsim</p>
-        <div class="myprofile_info_edit"><i class="fa fa-edit"></i> <form id="myprofile_info_edit"><input placeholder="Erkan Öğümsöğütlü" /></form></div>
-        </div>
-        
-        <div class="myprofile_information_components">
-        <p>Kullanıcı Adı</p>
-        <div class="myprofile_info_edit"><i class="fa fa-edit"></i> <form id="myprofile_info_edit"><input placeholder="erkanogumsogutlu" /></form></div>
-        </div>
-        
-        <div class="myprofile_information_components">
-        <p>E-Mail Adres</p>
-        <div class="myprofile_info_edit"><i class="fa fa-edit"></i> <form id="myprofile_info_edit"><input placeholder="erkan@linden-tech.com" /></form></div>
-        </div>
-        
-        
-        <div class="myprofile_information_components">
-        
-        <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                <h3 class="panel-title"> <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Şifreni Değiştir</a> </h3>
-                </div>
-                <div id="collapseThree" class="panel-collapse collapse">
-                  <div class="panel-body"> 
-                        <div><p>Eski Şifre:</p> <input name="LoginForm[password]" id="LoginForm_password" type="password"></div><br />
-                        <div><p>Yeni Şifre:</p> <input name="LoginForm[password]" id="LoginForm_password" type="password"></div><br />
-                        <div> <p>Yine Şifre Tekrarı:</p> <input name="LoginForm[password]" id="LoginForm_password" type="password"></div>
-                  </div>
-                </div>
+ 
+        <div class="account_info_cards_container">
+            <div class="account_info_cards">
+            <div class="account_info_icon"><i class="fa fa-book"></i></div>
+            <div class="account_info_data_number">17</div>
+            <div class="account_info_data_type">Kitap</div>
+            </div>
+            
+            <div class="account_info_cards">
+            <div class="account_info_icon"><i class="fa fa-suitcase"></i></div>
+            <div class="account_info_data_number">4</div>
+            <div class="account_info_data_type">Çalışma Alanı</div>
+            </div>
+            
+            <div class="account_info_cards">
+            <div class="account_info_icon"><i class="fa fa-desktop"></i></div>
+            <div class="account_info_data_number">1</div>
+            <div class="account_info_data_type">Sunucu</div>
+            </div>
+            
+            <div class="account_info_cards">
+            <div class="account_info_icon"><i class="fa fa-file-text"></i></div>
+            <div class="account_info_data_number">8</div>
+            <div class="account_info_data_type">Yayın Kategorisi</div>
+            </div>
+            
+            <div class="account_info_cards">
+            <div class="account_info_icon"><i class="fa fa-turkish-lira"></i></div>
+            <div class="account_info_data_number">5</div>
+            <div class="account_info_data_type">Yayın Üretme Bütçesi</div>
             </div>
         </div>
-        
-        </div>
-        </div>
-        <!-- end of myprofile_information_part -->
-
+        <!-- end of account_info_cards_container -->
 
 	</div>
 </div>
