8498d8fa6e76737ca5f4cd3ebd19a4b100e120b2
Bekir ATAY <batay.44@gmail.com>
2014-06-02 08:55:10 +0300

    edit organisation title added to kontol paneli

--- protected/views/organisations/account.php
+++ protected/views/organisations/account.php
@@ -110,5 +110,59 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-3">
+            <div class="box border green">
+                <div class="box-title">
+                    <h4><i class="fa fa-briefcase"></i><?php echo $organisation->organisation_name ?></h4>
+                </div>    
+                <div class="box-body">
+                    <a class="btn btn-primary" href="#"  data-toggle="modal" data-id="organisationTitleModal" data-target="#organisationTitleModal"> <i class="fa fa-edit"> </i> <?php _e('İsim Değiştir'); ?></a>
+                </div>
+            </div>
+        <div>
     </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="organisationTitleModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel"><?php _e("Organizasyon"); ?></h4>
+      </div>
+      <div class="modal-body">
+        <?php _e("Organizasyon İsmi: "); ?>
+      <input type="text" name="organisationTitle" id="organisationTitle" value="<?php echo $organisation->organisation_name; ?>">
+      <br>
+      <div class="alert alert-danger" id="organisationTitleFeedback" style="display:none">
+          
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id="changeTitle"><?php _e("Kaydet") ?></a>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><?php _e("Vazgeç") ?></button>      
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+    $("#changeTitle").click(function(){
+        var title=$("#organisationTitle").val();
+        var organisation="<?php echo $organisation->organisation_id; ?>";
+        $.ajax({
+              type: "POST",
+              data: {title: title, organisation:organisation},
+              url: '/organisations/changeTitle',
+            }).done(function(res){
+                console.log(res);
+                if (res=="0") {
+                    window.location.reload();
+                }
+                else{
+                    $("#organisationTitleFeedback").show();
+                    $("#organisationTitleFeedback").text("Beklenmeyen bir hata oluştu. Lütfen tekrar deneyiniz.");
+                };
+            });
+    });
+</script>
