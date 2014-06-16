80e93966b1103b83900fa2b44a0b6fd5f3d080a9
egemen <egemen@linden-tech.com>
2014-06-16 15:07:49 +0300

    pdf stuff

--- protected/components/EpubConverter.php
+++ protected/components/EpubConverter.php
@@ -20,17 +20,18 @@ class EpubConverter
 		$pdf='/tmp/'.$extract_folder;
 		exec('unzip -o -j '.$this->epub_path.' -d '.$pdf);
 		$files=array();
-		$this->getFiles($pdf.'/package.opf',$files);		
+		$this->getFiles($pdf.'/package.opf',$files);
+		error_log(print_r($files,1));		
 		if($this->process_count>sizeof($files))
 		{
 			$this->process_count=sizeof($files);
 			error_log("EpubConverter:number of processes should be less than number of pages but it is ok and fixed!\n");
 		}
 		$counter=(int)((sizeof($files)/$this->process_count));
-		for($i=0;$i< ($this->process_count)-1;$i++)
+		for($i=0;$i< ($this->process_count);$i++)
 			{
 				error_log("EpubConverter:process number: $i\n");
-				if($i==$this->process_count-2)
+				if($i==$this->process_count-1)
 				{
 					$temp_files=array_slice($files,$start_point);
 				}
@@ -74,14 +75,17 @@ class EpubConverter
 			{
 				
 				$href=(string)$page_attributes['href'][0];
-				
-				if($id=='titlepage')
-				{
-					$files=array_merge(array($href),$files);
-				}
-				else
-				{
-					$files[]=$href;
+				error_log($href);
+				error_log('\n');
+				if($href!='toc.xhtml'){
+					if($id=='titlepage')
+					{
+						$files=array_merge(array($href),$files);
+					}
+					else
+					{
+						$files[]=$href;
+					}
 				}
 			}
 			
